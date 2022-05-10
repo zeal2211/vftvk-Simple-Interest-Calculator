608860b343f77d08f9a19ce4bfdfab6c0c14ca16
Harish Nair <57914901+zeal2211@users.noreply.github.com>
2022-05-10 20:12:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if(principal=="" || principal<1){
+        alert("Enter a positive number");
+        return document.getElementById("principal").focus();
+    }
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate/ 100;
-    var amount = principal + interest
     var year = new Date().getFullYear()+parseInt(years);
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
-
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+interest+",\<br\>in the year "+year+"\<br\>"
+    
 }
 function updateRate(){
     var rateval = document.getElementById("rate").value;
